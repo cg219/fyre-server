ad477d5e03ed979845007333d7105dd4c4b64353
Clemente <dev@imkreative.com>
2020-07-18 19:11:23 -0400

    Add Stock Lookup

--- credentials.js
+++ credentials.js
@@ -1,3 +1,5 @@
 module.exports = {
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    FINNHUB_TOKEN: process.env.FINNHUB_TOKEN,
+    API_URL: process.env.API_URL
 }
--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const KoaRouter = require("@koa/router");
 const KoaBody = require("koa-bodyparser");
 const KoaJSON = require("koa-json");
 const KoaCORS = require("@koa/cors");
+const axios = require("axios");
 const path = require("path");
 const PORT = credentials.PORT;
 const app = new Koa();
 const router = new KoaRouter();
-
 const errorResponse = (ctx, error) => ctx.body = { status: 400, message: error.message };
+const api = axios.create({ baseURL: process.env.API_URL });
 
-router.get("/", async ctx => {
+router.get("/api/:symbol", async ctx => {
     try {
-        ctx.body = { status: 200, data: {} }
+        let price = await api.get("quote", { params: { symbol: ctx.params.symbol.toUpperCase(), token: credentials.FINNHUB_TOKEN } });
+
+        if (price.data.error) {
+            errorResponse(ctx, { message: price.data.error});
+        } else {
+            ctx.body = { status: 200, data: { symbol: ctx.params.symbol.toUpperCase(), price: price.data.c } }
+        }
     } catch (error) {
-        errorResponse(cta, error);
+        errorResponse(ctx, error);
     }
 })
 
