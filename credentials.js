03d8a0e7d767e6ed336d7bff23e8d22aa7367d3c
Clemente <dev@imkreative.com>
2020-07-19 04:04:13 -0400

    Integrate Firebase DB

--- credentials.js
+++ credentials.js
@@ -1,4 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    API_URL: process.env.API_URL
+    API_URL: process.env.API_URL,
+    FIREBASE_SERVICE_ACCOUNT: process.env.FIREBASE_SERVICE_ACCOUNT
 }
--- index.js
+++ index.js
@@ -3,33 +3,104 @@ const Koa = require("koa");
 const KoaRouter = require("@koa/router");
 const KoaBody = require("koa-bodyparser");
 const KoaJSON = require("koa-json");
-const KoaCORS = require("@koa/cors");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const admin = require("firebase-admin");
 const path = require("path");
 const PORT = credentials.PORT;
+const LOOKUP_BUFFER = 15000;
 const app = new Koa();
 const router = new KoaRouter();
 const errorResponse = (ctx, error) => ctx.body = { status: 400, message: error.message };
 const api = axios.create({ baseURL: process.env.API_URL });
 
-router.get("/apitest/:symbol", async ctx => {
+const initFirestore = () => {
+    const buffer = Buffer.from(credentials.FIREBASE_SERVICE_ACCOUNT, "base64");
+    const auth = JSON.parse(buffer.toString("utf-8"));
+
+    admin.initializeApp({ credential: admin.credential.cert(auth) });
+
+    return admin.firestore();
+}
+
+const checkStore = async (symbol) => {
     try {
-        let { data } = await test.get(`${ctx.params.symbol}/`);
+        const stock = await firestore.collection("stocks").doc(symbol).get();
+
+        if (stock.exists) {
+            return stock.data();
+        }
+
+        return false;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+const fetchStock = async (symbol) => {
+    try {
+        let { data } = await api.get(`${symbol}/`);
         let c = cheerio.load(data);
         let unrefinedPrice = c(".current-price").text();
         let priceCheck = /([0-9\.\,]+).+/
         let refinedPrice = unrefinedPrice.replace(priceCheck, "$1").replace(",", "");
-        let price = Number(refinedPrice)
 
-        ctx.body = { status: 200, data: { symbol: ctx.params.symbol, price }};
+        return Number(refinedPrice);
+
+    } catch (error) {
+        console.error(error);
+        throw Error("Stock Not Found");
+    }
+}
+
+const storeStock = async (symbol, price) => {
+    try {
+        const stock = {
+            symbol,
+            price,
+            date: Date.now()
+        }
+
+        const doc = await firestore.collection("stocks").doc(symbol).set(stock);
+
+        return true;
+    } catch (error) {
+        console.error(error);
+        throw Error("Error Storing Stock");
+    }
+}
+
+const firestore = initFirestore();
+
+router.get("/api/:symbol", async ctx => {
+    try {
+        const symbol = ctx.params.symbol;
+        const store = await checkStore(symbol);
+        let price;
+        let shouldStore = true
+        let didStore;
+
+        if (store) {
+            if (Date.now() - store.date < LOOKUP_BUFFER) {
+                price = store.price;
+                shouldStore = false;
+            } else {
+                price = await fetchStock(symbol);
+            }
+        } else {
+            price = await fetchStock(symbol);
+        }
+
+        ctx.body = { status: 200, data: { symbol, price }};
+
+        if (shouldStore) { storeStock(symbol, price) }
     } catch (error) {
         errorResponse(ctx, error);
     }
 })
 
 app
-    .use(KoaCORS())
     .use(KoaBody())
     .use(KoaJSON())
     .use(router.routes())
