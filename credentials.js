913423fe4a1be56ad023fdb8496cb3c8c81c3984
Clemente <dev@imkreative.com>
2020-07-18 21:24:32 -0400

    Webscrape Replacement

--- credentials.js
+++ credentials.js
@@ -1,6 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    FINNHUB_TOKEN: process.env.FINNHUB_TOKEN,
-    API_URL: process.env.API_URL,
-    SCRAPE_URL: process.env.SCRAPE_URL
+    API_URL: process.env.API_URL
 }
--- index.js
+++ index.js
@@ -5,33 +5,26 @@ const KoaBody = require("koa-bodyparser");
 const KoaJSON = require("koa-json");
 const KoaCORS = require("@koa/cors");
 const axios = require("axios");
+const cheerio = require("cheerio");
 const path = require("path");
 const PORT = credentials.PORT;
 const app = new Koa();
 const router = new KoaRouter();
 const errorResponse = (ctx, error) => ctx.body = { status: 400, message: error.message };
 const api = axios.create({ baseURL: process.env.API_URL });
-const test = axios.create({ baseURL: process.env.SCRAPE_URL });
-
-router.get("/api/:symbol", async ctx => {
-    try {
-        let price = await api.get("quote", { params: { symbol: ctx.params.symbol.toUpperCase(), token: credentials.FINNHUB_TOKEN } });
-
-        if (price.data.error) {
-            errorResponse(ctx, { message: price.data.error});
-        } else {
-            ctx.body = { status: 200, data: { symbol: ctx.params.symbol.toUpperCase(), price: price.data.c } }
-        }
-    } catch (error) {
-        errorResponse(ctx, error);
-    }
-})
 
 router.get("/apitest/:symbol", async ctx => {
     try {
+        let { data } = await test.get(`${ctx.params.symbol}/`);
+        let c = cheerio.load(data);
+        let unrefinedPrice = c(".current-price").text();
+        let priceCheck = /([0-9\.\,]+).+/
+        let refinedPrice = unrefinedPrice.replace(priceCheck, "$1").replace(",", "");
+        let price = Number(refinedPrice)
 
+        ctx.body = { status: 200, data: { symbol: ctx.params.symbol, price }};
     } catch (error) {
-        errorResponse(ctx, error)
+        errorResponse(ctx, error);
     }
 })
 
